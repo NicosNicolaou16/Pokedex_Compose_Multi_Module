e5d8976dc978b76411df8870a11a695b488022a8
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-09-01 13:14:22 +0300

    fix the dependencies

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.dagger.android)
     ksp(libs.dagger.compiler)
     ksp(libs.hilt.compiler)
+    //Unit Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
