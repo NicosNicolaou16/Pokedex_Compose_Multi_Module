61e3a34c146c593c2dcb36bb8f3b3ba1afc579f1
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-08-17 01:11:08 +0300

    minor change

--- database/src/main/java/com/nicos/database/data/room_database/init_database/BaseDao.kt
+++ database/src/main/java/com/nicos/database/data/room_database/init_database/BaseDao.kt
@@ -6,4 +6,13 @@ interface BaseDao<O, L> {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertOrReplaceObject(data: O)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrReplaceList(data: L)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateObject(data: O)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateList(data: L)
 }
