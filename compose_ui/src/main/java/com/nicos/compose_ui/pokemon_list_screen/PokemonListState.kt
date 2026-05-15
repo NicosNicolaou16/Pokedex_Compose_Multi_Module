aed4c6763aae62ef354343a5438a53b3c2d4a69b
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-05-16 00:45:50 +0300

    fix coroutine dispatchers in View Model

--- compose_ui/src/main/java/com/nicos/compose_ui/pokemon_list_screen/PokemonListState.kt
+++ compose_ui/src/main/java/com/nicos/compose_ui/pokemon_list_screen/PokemonListState.kt
@@ -1,7 +1,6 @@
 package com.nicos.compose_ui.pokemon_list_screen
 
 import com.nicos.core.domain.PokemonUi
-import com.nicos.database.data.room_database.entities.PokemonEntity
 
 data class PokemonListState(
     val pokemonMutableList: MutableList<PokemonUi>? = null,
