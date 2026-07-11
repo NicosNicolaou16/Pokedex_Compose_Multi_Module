d4c6b54942f0c1adec2f2d52fef45382e5e43361
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-07-12 01:03:27 +0300

    migration to Room 3

--- database/src/main/java/com/nicos/database/data/room_database/entities/PokemonEntity.kt
+++ database/src/main/java/com/nicos/database/data/room_database/entities/PokemonEntity.kt
@@ -1,7 +1,7 @@
 package com.nicos.database.data.room_database.entities
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room3.Entity
+import androidx.room3.PrimaryKey
 import com.nicos.network.data.dto.PokemonDto
 
 @Entity
