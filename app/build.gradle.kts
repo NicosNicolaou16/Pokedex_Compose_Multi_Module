98495e75da70e82f3a984c2e7f7570702f6a5d34
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-07-12 00:40:10 +0300

    update Target SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 android {
     namespace = "com.nicos.pokedex_compose_multi_module"
     buildToolsVersion = "37.0.0"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.nicos.pokedex_compose_multi_module"
