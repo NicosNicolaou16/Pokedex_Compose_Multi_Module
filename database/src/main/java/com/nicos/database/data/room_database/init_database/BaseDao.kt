b5615a56414c2b4ce3aa46445de95a08afa89a10
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-07-12 01:05:31 +0300

    migration to Room 3

--- database/src/main/java/com/nicos/database/data/room_database/init_database/BaseDao.kt
+++ database/src/main/java/com/nicos/database/data/room_database/init_database/BaseDao.kt
@@ -1,6 +1,8 @@
 package com.nicos.database.data.room_database.init_database
 
-import androidx.room.*
+import androidx.room3.Insert
+import androidx.room3.OnConflictStrategy
+import androidx.room3.Update
 
 interface BaseDao<O, L> {
 
