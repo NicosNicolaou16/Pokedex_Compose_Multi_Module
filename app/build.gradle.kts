bd955590687a8eca1650a5114f2d80b3801ba327
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-07-12 00:40:18 +0300

    update Target SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.nicos.pokedex_compose_multi_module"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
